f769d8ac16248485956b27fac0371ff7987d314d
Anna De Lise <anna_delise@libero.it>
2025-07-31 15:54:20 +0200

    add filtered test

--- src/tests/BookList.test.jsx
+++ src/tests/BookList.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
 import BookList from '../components/BookList'
 import scifiBooks from '../assets/data/scifi.json'
@@ -9,4 +9,14 @@ describe('test initial mounting', () => {
     const cards = screen.getAllByTestId('cardBook')
     expect(cards).toHaveLength(scifiBooks.length)
   })
+  it('Filters books based on search input', async () => {  
+    render(<BookList arrayBook={scifiBooks} />)
+    const searchInput = screen.getByPlaceholderText('Cerca..')
+    fireEvent.change(searchInput, { target: { value: 'pandemic' } })
+    const filteredCards = await screen.findAllByTestId('cardBook')
+    expect(filteredCards.length).toBeLessThan(scifiBooks.length)
+    expect(filteredCards).toHaveLength(1)
+    expect(screen.getByText(/pandemic/i)).toBeInTheDocument()
+  })
+
 })
